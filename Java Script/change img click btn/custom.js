b5053f43113e81b1c3cac1c4a74d2b3afb255950
krupal27 <krupalpatel2707@gmail.com>
2023-09-04 11:22:08 +0530

    change image on click

---
+++ Java Script/change img click btn/custom.js
@@ -0,0 +1,7 @@
+ function btnclickhandle(params) {
+            console.log("called");
+        }
+
+        document.getElementById("btn").addEventListener("click",function () { //anoynemus function without name 
+            console.log("called");
+        })
