fdee73e058c15b37b9abab0ded1261fbc50f5ef7
krupal27 <krupalpatel2707@gmail.com>
2023-12-19 16:22:33 +0530

    React-js

--- React-js/my-app/src/Component/Classcomponent/Classcompomenu.jsx
+++ React-js/my-app/src/Component/Classcomponent/Classcompomenu.jsx
@@ -1,1 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+class Classcompomenu extends Component {
+    render() {
+        return (
+            <>
+                <ul>
+                    <li><Link to="Classcompointro">Classcompointro</Link></li>
+                    <li><Link to="Classone">Classone</Link></li>
+                </ul>
+                <Outlet/>
+            </>
+        )
+    }
+}
+
+export default Classcompomenu
