852f5abe194e1b7ee5d440c8c4aed59df953e57d
krupal27 <krupalpatel2707@gmail.com>
2023-12-23 14:16:02 +0530

    React-js Assignment

---
+++ Assinment/React Assinment/React Router/my-project/src/Components/Functioncompo/Functioncompomenu.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+
+function Functioncompomenu (){
+    return (
+        <>
+        
+
+
+        </>
+    )
+}
+
+export default Functioncompomenu
---
+++ Assinment/React Assinment/React Router/my-project/src/Components/Functioncompo/Functioncomporouter.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Component } from 'react';
+
+function Functioncomporouter() {
+    return ( 
+        <>
+        
+
+
+        </>
+     );
+}
+
+export default Functioncomporouter;
