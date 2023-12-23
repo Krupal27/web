4bbd25a4de1772e1d6796b74c3c32f0f03830c25
krupal27 <krupalpatel2707@gmail.com>
2023-12-23 14:39:54 +0530

    React-js Assignment

--- Assinment/React Assinment/React-js/my-project/src/Components/Classcompo/03Classcompostate.jsx
+++ Assinment/React Assinment/React-js/my-project/src/Components/Classcompo/03Classcompostate.jsx
@@ -2,15 +2,34 @@ import React from 'react';
 import { Component } from 'react';
 
 class Classcompostate extends Component {
-    render() { 
+
+    constructor() {
+        super()
+        this.state = {
+            count: 0
+        }
+    }
+
+    increase = () => {
+        console.log("Increase");
+        this.setState({count: this.state.count + 1})
+    }
+    decrease = () => {
+        console.log("Decrease");
+        this.setState({count: this.state.count - 1})
+    }
+
+    render() {
         return (
             <>
-            
-            <h1>ClasscompoState</h1>
 
+                <h1>ClasscompoState</h1>
+                <h3>{this.state.count}</h3>
+                <button onClick={this.increase} className='mx-2'>Increase</button>
+                <button onClick={this.decrease}>Decrease</button>
             </>
         );
     }
 }
- 
+
 export default Classcompostate;
