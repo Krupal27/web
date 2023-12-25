74129369a24bae2b416db09faac96ab891bc9c0d
krupal27 <krupalpatel2707@gmail.com>
2023-12-25 16:41:47 +0530

    Project 1

--- Project/Singal page/app.js
+++ Project/Singal page/app.js
@@ -1,1 +1,14 @@
+const targetimage=document.getElementById("mainproduct")
 
+let children = document.getElementsByClassName("multiimg");
+ function handleClick(event) {
+    console.log(event.target.tagName);
+    if (event.target.tagName.toLowerCase() === 'img') {
+      var srcPath = event.target.src;
+      targetimage.setAttribute("src",srcPath)
+    }
+  }
+
+  for (var i = 0; i < children.length; i++) {
+    children[i].addEventListener('click', handleClick);
+  }
