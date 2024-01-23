965ecbb8267b9140eed40dbb566e33576874d6b7
krupal27 <krupalpatel2707@gmail.com>
2024-01-23 16:19:44 +0530

    React js

--- React-js/my-app/src/Mainrouter.jsx
+++ React-js/my-app/src/Mainrouter.jsx
@@ -42,7 +42,7 @@ const Mainrouter = createBrowserRouter([
             },
             {
                 path: "Functioncomponent/*",
-                element:<><Functioncomporoutes/></>
+                element: <><Functioncomporoutes /></>
             }
         ]
     }
