c0e8f05c13703d944c5758b1fcae2ec37d636f19
krupal27 <krupalpatel2707@gmail.com>
2024-01-24 15:54:00 +0530

    React js

--- React-js/my-app/src/Component/Example.jsx
+++ React-js/my-app/src/Component/Example.jsx
@@ -8,19 +8,19 @@ function Example() {
             <h1>Example</h1>
             <div className="container my-3">
                 <div className="row">
-                    <div className="col">
+                    <div className="col-6">
                         <h3>
                             <Link to="/Example/Classcomponent">Class Component</Link>
                         </h3>
                     </div>
-                    <div className="col">
+                    <div className="col-6">
                         <h3>
                             <Link to="/Example/Functioncomponent">Function Component</Link>
                         </h3>
                     </div>
                 </div>
                 <div className="row my-3">
-                    <div className="col">
+                    <div className="col-12">
                         <Outlet />
                     </div>
                 </div>
