1bd265ba660c0e245b655038dd80b890962bf2bc
krupal27 <krupalpatel2707@gmail.com>
2024-01-31 22:22:03 +0530

    React js

--- React-js/my-app/src/Component/Functioncomponent/07Api.jsx
+++ React-js/my-app/src/Component/Functioncomponent/07Api.jsx
@@ -1,18 +1,44 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
 
 const Api = () => {
-    return ( 
+    const [userdata, setUserdata] = useState()
+    useEffect(() => {
+        fetch("https://jsonplaceholder.typicode.com/todos").then((resp) => {
+            resp.json().then((result) => {
+                // console.log(result);
+                setUserdata(result);
+            })
+        })
+    },[]);
+    console.log(userdata);
+    return (
         <>
-        
-        <h1>Api</h1>
+
+            <h1>Api</h1>
+
+            <table width="100%" border="1">
+                <tr>
+                    <td>userId</td>
+                    <td>id</td>
+                    <td>title</td>
+                    <td>completed</td>
+                </tr>
+                {
+                    userdata && userdata.map((item) =>
+                        <tr key={item}>
+                            <td>{item.userId}</td>
+                            <td>{item.id}</td>
+                            <td>{item.title}</td>
+                            <td>{item.completed.toString()}</td>
+                        </tr>
+                    )
+                }
+            </table>
+
 
         </>
-     );
+    );
 }
- 
+
 export default Api;
-<>
-
-<h1>Api</h1>
-
-</>
