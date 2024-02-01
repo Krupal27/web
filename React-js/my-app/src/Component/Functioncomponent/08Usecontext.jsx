b711223e2147d1dcbdefa42246805102ece02ac1
krupal27 <krupalpatel2707@gmail.com>
2024-02-01 10:00:57 +0530

    React js

--- React-js/my-app/src/Component/Functioncomponent/08Usecontext.jsx
+++ React-js/my-app/src/Component/Functioncomponent/08Usecontext.jsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import CompoA from './CompoA';
+import { createContext } from 'react';
+
+const Fname = createContext()
 
 const Usecontaext = () => {
-    let test = "123"
+    let test = 'Hello'
     return (
         <>
 
             <h3>Usecontaext</h3>
-            <CompoA test={test} />
+            <Fname.Provider value={test}>
+                <CompoA />
+            </Fname.Provider>
 
         </>
     );
 }
 
 export default Usecontaext;
+export {Fname};
--- React-js/my-app/src/Component/Functioncomponent/CompoC.jsx
+++ React-js/my-app/src/Component/Functioncomponent/CompoC.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Fname } from './08Usecontext';
+import { useContext } from 'react';
 
-const CompoC = (props) => {
-    return ( 
+const CompoC = () => {
+    let Data = useContext(Fname); // Using Hook
+    return (
         <>
-        
-        <h1>data</h1>
-        {/* {props.test} */}
+
+            {/* <h1>data</h1> */}
+            <h1>{Data}</h1>
+            {/* <Fname.Consumer> // Using Api Process
+                {
+                    (number) => {
+                        return <h2>{number}</h2>
+                    }
+                }
+            </Fname.Consumer> */}
+            {/* {props.test} */}
         </>
-     );
+    );
 }
- 
+
 export default CompoC;
