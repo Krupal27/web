2df872bfb8a066e71cd930f0a737e5224383229b
krupal27 <krupalpatel2707@gmail.com>
2024-02-07 22:15:05 +0530

    React js CRUD

--- React-js/react-crud/src/Component/Userdata.jsx
+++ React-js/react-crud/src/Component/Userdata.jsx
@@ -1,13 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { MDBTable, MDBTableHead, MDBTableBody } from 'mdb-react-ui-kit';
+import { useNavigate } from 'react-router-dom';
 
 const Userdata = () => {
-    return ( 
+
+    const [userdata, setUserdata] = useState("")
+
+    useEffect(() => {
+        fetch("http://localhost:7501/userdata").then((resp) => {
+            resp.json().then((result) => {
+                // console.log(result);
+                setUserdata(result)
+            })
+        })
+    }, []);
+    console.log(userdata);
+
+    let navigate = useNavigate()
+
+    let useraddnav = () => {
+        navigate("/useradd")
+    }
+
+    return (
         <>
-        
-        <h3>Userdata</h3>
+
+            <h3>Userdata</h3>
+
+            <button className='mx-3 my-4 bg-black text-white rounded-3' onClick={useraddnav}>Useradd</button>
+
+            <MDBTable>
+                <MDBTableHead>
+                    <tr>
+                        <th scope='col'>id</th>
+                        <th scope='col'>name</th>
+                        <th scope='col'>age</th>
+                        <th scope='col'>location</th>
+                        <th scope='col'>action</th>
+                    </tr>
+                    {
+                        userdata && userdata.map((val) =>
+                            <tr>
+                                <td>{val.id}</td>
+                                <td>{val.name}</td>
+                                <td>{val.age}</td>
+                                <td>{val.location}</td>
+                                <td>{val.action}</td>
+                            </tr>
+                        )
+                    }
+                </MDBTableHead>
+            </MDBTable>
+
+
+
 
         </>
-     );
+    );
 }
- 
+
 export default Userdata;
