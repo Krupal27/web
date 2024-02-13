a8add8335fdd5609fa99945a7d407c39a38baa6e
krupal27 <krupalpatel2707@gmail.com>
2024-02-13 15:12:34 +0530

    React Loin

---
+++ React-js/React-Login/src/Coman-compo/Navbar.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import {
+    MDBContainer,
+    MDBNavbar,
+    MDBNavbarBrand,
+    MDBNavbarToggler,
+    MDBIcon,
+    MDBNavbarNav,
+    MDBNavbarItem,
+    MDBNavbarLink,
+    MDBBtn,
+    MDBDropdown,
+    MDBDropdownToggle,
+    MDBDropdownMenu,
+    MDBDropdownItem,
+    MDBCollapse,
+} from 'mdb-react-ui-kit';
+import { Link } from 'react-router-dom';
+
+export default function Navbar() {
+    const [openBasic, setOpenBasic] = useState(false);
+
+    return (
+        <MDBNavbar expand='lg' light bgColor='light'>
+            <MDBContainer fluid>
+                <MDBNavbarBrand href='#'>Logo</MDBNavbarBrand>
+
+                <MDBNavbarToggler
+                    aria-controls='navbarSupportedContent'
+                    aria-expanded='false'
+                    aria-label='Toggle navigation'
+                    onClick={() => setOpenBasic(!openBasic)}
+                >
+                    <MDBIcon icon='bars' fas />
+                </MDBNavbarToggler>
+
+            </MDBContainer>
+                <MDBCollapse navbar open={openBasic}>
+                    <MDBNavbarNav className='mr-auto mb-2 mb-lg-0'>
+                        <MDBNavbarItem>
+                            <MDBNavbarLink active aria-current='page' href='#'>
+                                <Link to="/registration">Registration</Link>
+                            </MDBNavbarLink>
+                        </MDBNavbarItem>
+                        <MDBNavbarItem>
+                            <MDBNavbarLink active aria-current='page' href='#'>
+                                <Link to="/login">Login</Link>
+                            </MDBNavbarLink>
+                        </MDBNavbarItem>
+                    </MDBNavbarNav>
+                </MDBCollapse>
+        </MDBNavbar>
+    );
+}
