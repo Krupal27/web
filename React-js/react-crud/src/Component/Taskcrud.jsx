3ad95af8ac3ff90b6c9aa31ef3a1464d3f0eb681
krupal27 <krupalpatel2707@gmail.com>
2024-02-16 15:54:21 +0530

    React CRUD

---
+++ React-js/react-crud/src/Component/Taskcrud.jsx
@@ -0,0 +1,63 @@
+import React, { useState } from 'react';
+
+const Taskcrud = () => {
+
+    const [name, setName] = useState("")
+    const [alldata, setAlldata] = useState([])
+    const [show, setShow] = useState(false)
+    const [edittext, setEdittext] = useState()
+
+    let adddata = () => {
+        console.log(name);
+        setAlldata((newdata) => [...newdata, name])
+        setName("")
+    }
+
+    let Delete = (i) => {
+        console.log(i);
+        alldata.splice(i, 1)
+        setAlldata([...alldata])
+    }
+
+    let Edit = (i) => {
+        console.log(i);
+        setName(alldata[i])
+        setShow(true)
+        setEdittext(i)
+    }
+
+    let updatedata = () => {
+        console.log("update", edittext);
+        alldata.splice(edittext, 1, name)
+        setAlldata([...alldata])
+        setName("")
+        setShow(false)
+    }
+
+    console.log(alldata);
+    console.log(edittext);
+    return (
+        <>
+
+            <h3>Taskcrud</h3>
+            <div>
+                <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
+                {!show ? <button onClick={adddata}>Adddata</button> :
+                    <button onClick={updatedata}>Updatedata</button>}
+            </div>
+            {
+                alldata.map((value, i) =>
+                    <>
+                        <h3>{value}</h3>
+                        <button onClick={() => Edit(i)}>Edit</button>
+                        <button onClick={() => Delete(i)}>Delete</button>
+                    </>
+
+                )
+            }
+
+        </>
+    );
+}
+
+export default Taskcrud;
