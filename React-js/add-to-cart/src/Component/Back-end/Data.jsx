cc27de395d63d97243f3347748bcdd49d1d3c1bf
krupal27 <krupalpatel2707@gmail.com>
2024-02-20 18:55:52 +0530

    React add-to-cart

---
+++ React-js/add-to-cart/src/Component/Back-end/Data.jsx
@@ -0,0 +1,102 @@
+import img1 from "../Back-end/assets/f1.jpg"
+import img2 from "../Back-end/assets/f2.jpg"
+import img3 from "../Back-end/assets/f3.jpg"
+import img4 from "../Back-end/assets/f4.jpg"
+import img5 from "../Back-end/assets/f5.jpg"
+import img6 from "../Back-end/assets/f6.jpg"
+import img7 from "../Back-end/assets/n1.jpg"
+import img8 from "../Back-end/assets/n2.jpg"
+import img9 from "../Back-end/assets/n3.jpg"
+import img10 from "../Back-end/assets/n4.jpg"
+import img11 from "../Back-end/assets/n5.jpg"
+import img12 from "../Back-end/assets/n8.jpg"
+
+
+const list = [
+    {
+        id:1,
+        name: "Cartoon Astronaut T-Shirt",
+        price:"78",
+        amount:"1",
+        img:`${img1}`
+    },
+    {
+        id:2,
+        name: "Cartoon Astronaut T-Shirt",
+        price:"78",
+        amount:"1",
+        img:`${img2}`
+    },
+    {
+        id:3,
+        name: "Cartoon Astronaut T-Shirt",
+        price:"78",
+        amount:"1",
+        img:`${img3}`
+    },
+    {
+        id:4,
+        name: "Cartoon Astronaut T-Shirt",
+        price:"78",
+        amount:"1",
+        img:`${img4}`
+    },
+    {
+        id:5,
+        name: "Cartoon Astronaut T-Shirt",
+        price:"78",
+        amount:"1",
+        img:`${img5}`
+    },
+    {
+        id:6,
+        name: "Cartoon Astronaut T-Shirt",
+        price:"78",
+        amount:"1",
+        img:`${img6}`
+    },
+    {
+        id:7,
+        name: "Cartoon Astronaut T-Shirt",
+        price:"78",
+        amount:"1",
+        img:`${img7}`
+    },
+    {
+        id:8,
+        name: "Cartoon Astronaut T-Shirt",
+        price:"78",
+        amount:"1",
+        img:`${img8}`
+    },
+    {
+        id:9,
+        name: "Cartoon Astronaut T-Shirt",
+        price:"78",
+        amount:"1",
+        img:`${img9}`
+    },
+    {
+        id:10,
+        name: "Cartoon Astronaut T-Shirt",
+        price:"78",
+        amount:"1",
+        img:`${img10}`
+    },
+    {
+        id:11,
+        name: "Cartoon Astronaut T-Shirt",
+        price:"78",
+        amount:"1",
+        img:`${img11}`
+    },
+    {
+        id:12,
+        name: "Cartoon Astronaut T-Shirt",
+        price:"78",
+        amount:"1",
+        img:`${img12}`
+    },
+]
+
+export default list
