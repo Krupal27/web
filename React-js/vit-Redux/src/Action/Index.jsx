9129f8702fc132a852fd5a61389416f8a7ff257d
krupal27 <krupalpatel2707@gmail.com>
2024-02-28 15:53:39 +0530

    React Redux

---
+++ React-js/vit-Redux/src/Action/Index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export const incNumber = () => {
+    return {
+        typeof: "INCREMENT"
+    }
+}
+
+export const decNumber = () => {
+    return {
+        typeof: "DECREMENT"
+    }
+}
+
