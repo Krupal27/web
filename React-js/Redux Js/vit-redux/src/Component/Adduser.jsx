3ce8a163789cbc064fd13a3382b03970a166efd8
krupal27 <krupalpatel2707@gmail.com>
2024-03-14 09:50:22 +0530

    Redux js

---
+++ React-js/Redux Js/vit-redux/src/Component/Adduser.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { Col, Container, Form, Row } from 'react-bootstrap'
+
+function Adduser() {
+    return (
+
+        <>
+
+            <Container>
+                <Row>
+                    <Col xs={4}>
+                        <Image />
+                    </Col>
+                    <Col xs={6}>
+                        <Form>
+
+                        </Form>
+                    </Col>
+                </Row>
+            </Container>
+
+        </>
+    )
+}
+
+export default Adduser
