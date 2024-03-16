be96e938c9fd9d9c6799352a76f01ffa79642afe
krupal27 <krupalpatel2707@gmail.com>
2024-03-16 12:02:36 +0530

    Java Script

---
+++ Java Script Project/Java Script Interview Queastions/app.js
@@ -0,0 +1,57 @@
+// 1 :- Reverce This String
+// let str = "hello ji kese ho";
+
+// let result = str.split(" ").map((e) => {
+//     return e.split("").reverse().join("")
+
+// })
+
+
+// console.log(result.join(" "));
+
+// 2 :- Chake IsArry
+
+// let isArrayChake = (ele) => {
+//     return Array.isArray(ele)
+// }
+
+// console.log(isArrayChake([]));
+// console.log(isArrayChake({}));
+
+
+// 3 :- How to Empty Array In java script
+
+// let Array = [1, 2, 3, 4, 5]
+
+// Array.length = 0;
+// console.log(Array.length);
+// console.log(Array);
+
+
+// 4 :- How would you chake if a number is Integer ?
+
+// let a = 12.0;
+
+// if (a % 1 == 0) {
+//     console.log("Integer");
+// } else {
+//     console.log("Not !");
+// }
+
+
+// 5 :- Concate array
+
+// let arr = [1, 2, 3, 4, 5]
+// let result = arr.concat(arr);
+// console.log(result);
+
+// function concateArray() {
+//     return arr.concat(arr)
+// }
+// console.log(concateArray());
+
+// 6 :- Reverse Number
+
+// let NumberConvert = ()=>{
+
+// }
